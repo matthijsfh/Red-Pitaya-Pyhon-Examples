4bc612a45079073028f184f234955cb5052ad968
matthijs hajer <matthijsfh@planet.nl>
2022-12-14 14:30:26 +0100

    Added bar graphs to datastorage

--- datastorage/datastorage_class.py
+++ datastorage/datastorage_class.py
@@ -10,6 +10,8 @@ import json
 class datastorage_class:
     name_list = {}
     title = []
+    xlabel = []
+    ylabel = []
 
     def __init__(self, name):
         self.name = name
@@ -23,6 +25,12 @@ class datastorage_class:
         self.name_list[in_name] = []
 
         return
+    
+    def add_x_label(self, inLabel):
+        self.xlabel = inLabel
+
+    def add_y_label(self, inLabel):
+        self.ylabel = inLabel
 
     def add_data(self, in_name, in_data, debug = 0):
         old_data = self.name_list[in_name]
@@ -51,6 +59,9 @@ class datastorage_class:
         ax.legend()
         ax.set(title=title)
         ax.grid(True)
+
+        ax.set(xlabel=self.xlabel, ylabel=self.ylabel)
+
         return
 
     def plot_data(self, ax, in_name_x, in_name_y, color=1, points_only=False, label='', title='', marker='', linewidth=1, x_offset=0):
@@ -65,6 +76,41 @@ class datastorage_class:
         ax.legend()
         ax.set(title=title)
         ax.grid(True)
+        
+        ax.set(xlabel=self.xlabel, ylabel=self.ylabel)
+        
+        return
+    
+    def plot_data_bars(self, ax, in_name_x, in_name_y, color=1, label='', title='', width=1, barcount=1, barID=1):
+        values_x = self.name_list[in_name_x]
+        values_y = self.name_list[in_name_y]
+
+        if (barcount == 1):
+            width = 0.5;
+            self.plot_bars(ax, values_x , values_y, color=color, label=label, title=title, width=width)
+
+        if (barcount == 2):
+            width = 0.33;
+            if (barID == 1):
+                self.plot_bars(ax, values_x - width/2, values_y, color=color, label=label, title=title, width=width)
+            if (barID == 2):
+                self.plot_bars(ax, values_x + width/2, values_y, color=color, label=label, title=title, width=width)
+
+        if (barcount == 3):
+            width = 0.25;
+            if (barID == 1):
+                self.plot_bars(ax, values_x - width, values_y, color=color, label=label, title=title, width=width)
+            if (barID == 2):
+                self.plot_bars(ax, values_x, values_y, color=color, label=label, title=title, width=width)
+            if (barID == 3):
+                self.plot_bars(ax, values_x + width, values_y, color=color, label=label, title=title, width=width)
+
+        ax.legend()
+        ax.set(title=title)
+        ax.grid(True)
+        
+        ax.set(xlabel=self.xlabel, ylabel=self.ylabel)
+        
         return
 
     # =================================================================================
@@ -83,7 +129,14 @@ class datastorage_class:
         ax.scatter(point_x, point_y, label=label, color=dict_val, marker=marker, linewidth=linewidth)
         ax.legend()
         return
-
+    
+    def plot_bars(self, ax, point_x, point_y, color=1, label='', title='', width=1):
+        colors = dict(mcolors.CSS4_COLORS, **mcolors.CSS4_COLORS)
+        dict_val = list(colors.values())[color]
+        ax.bar(point_x, point_y, label=label, color=dict_val, width=width)
+        ax.legend()
+        return
+    
     def save_data(self, filename):
         with open(filename, 'wb') as filehandle:
             # store the data as binary data stream
